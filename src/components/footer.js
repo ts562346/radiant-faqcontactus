d74f92b439b9711afeec7e89c0b17123660bc188
Abed Hasan <abedhasan79@gmail.com>
2023-05-27 13:00:46 -0400

    href

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,16 +13,16 @@ export default function Footer() {
                     <a href='https://www.facebook.com' className='me-4 text-reset'>
                         <MDBIcon fab icon="facebook-f" />
                     </a>
-                    <a href='' className='me-4 text-reset'>
+                    <a href='#' className='me-4 text-reset'>
                         <MDBIcon fab icon="twitter" />
                     </a>
-                    <a href='' className='me-4 text-reset'>
+                    <a href='#' className='me-4 text-reset'>
                         <MDBIcon fab icon="google" />
                     </a>
-                    <a href='' className='me-4 text-reset'>
+                    <a href='#' className='me-4 text-reset'>
                         <MDBIcon fab icon="instagram" />
                     </a>
-                    <a href='' className='me-4 text-reset'>
+                    <a href='#' className='me-4 text-reset'>
                         <MDBIcon fab icon="linkedin" />
                     </a>
                 </div>
